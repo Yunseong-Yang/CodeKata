46a8a6b98c12a623e2bb34a06a2dcaf69d88f3a4
YangYunSeong <151173925+Yunseong-Yang@users.noreply.github.com>
2026-03-05 09:45:05 +0900

    Add function to compute sum of divisors
    Implement a function to calculate the sum of divisors for a given integer.

---
+++ 26.03/05/14_sum_of_divisor.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n / 2; i++)
+    {
+        n % i == 0 ? answer += i : 0;
+    }
+    return answer + n;
+}
