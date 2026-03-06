a7be4361c5b98b40b5f06726ee5d504c347beb83
YangYunSeong <151173925+Yunseong-Yang@users.noreply.github.com>
2026-03-06 09:53:45 +0900

    Add solution for finding missing numbers
    Implement solution to find the missing numbers from 0 to 9 in a given vector.

---
+++ 26.03/06/28_add_missing_number.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/86051
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    
+    for(int i = 0; i < 10; i++)
+    {
+        if (find(numbers.begin(), numbers.end(), i) == numbers.end())
+        {
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
