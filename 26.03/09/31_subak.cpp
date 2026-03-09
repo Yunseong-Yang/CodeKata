c9c447bd22b7a443e496f230424b22cc08805af2
YangYunSeong <151173925+Yunseong-Yang@users.noreply.github.com>
2026-03-09 09:10:52 +0900

    Add solution function for '수박' string generation
    Implement solution function to generate '수박' pattern based on input number.

---
+++ 26.03/09/31_subak.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12922
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i = 0; i < n / 2; i++)
+    {
+        answer += "수박";
+    }
+    
+    if (n % 2 == 1)
+    {
+        answer += "수";
+    }
+    
+    return answer;
+}
