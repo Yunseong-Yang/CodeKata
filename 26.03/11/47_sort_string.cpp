092d09891fcc279b730f45361de2a66e0ae2a02d
YangYunSeong <151173925+Yunseong-Yang@users.noreply.github.com>
2026-03-11 09:37:41 +0900

    Add function to sort strings by character index
    Implement string sorting based on character at index n.

---
+++ 26.03/11/47_sort_string.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int index;
+
+bool compare(string a, string b){
+    return a[index] == b[index] ? a < b : a[index] < b[index];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    index = n;
+    
+    sort(strings.begin(), strings.end(), compare);
+    
+    return strings;
+}
