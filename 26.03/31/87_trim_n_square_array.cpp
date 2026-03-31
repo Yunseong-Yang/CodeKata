cf64b281991477d0e6be753e20679b7bac4a646b
YangYunSeong <151173925+Yunseong-Yang@users.noreply.github.com>
2026-03-31 09:26:12 +0900

    Fix loop condition in solution function

---
+++ 26.03/31/87_trim_n_square_array.cpp
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    for (long long i = left; i <= right; i++)
+    {
+        (i / n) <= (i % n) ? answer.push_back(i % n + 1) : answer.push_back(i / n + 1);
+    }
+    
+    return answer;
+}
