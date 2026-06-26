116ccfee63aad9ddd95c3c63f30f3a2e5c844daf
yss <today3504@gmail.com>
2026-06-26 09:31:39 +0900

    secret map solution

---
+++ algorithms/bit/140_secret_map.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string transition(int n, int m)
+{
+    string s;
+    while(m != 0)
+    {
+        s += to_string(m % 2);
+        m /= 2;
+    }
+    while(s.size() != n)
+    {
+        s = s + '0';
+    }
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.size(); ++i)
+    {
+        s[i] = s[i] == '0' ? ' ' : '#';
+    }
+    
+    return s;
+}
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        string s;
+        int m = arr1[i] | arr2[i];
+        
+        s = transition(n, m);
+        answer.push_back(s);
+    }
+    
+    return answer;
+}
